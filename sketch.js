4820446900e2d9e0a8955ddedb027f2be9a0ea67
AliAsger5253Tayyebi <76243812+AliAsger5253Tayyebi@users.noreply.github.com>
2021-05-01 23:12:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup (){
+    createCanvas (1750,1100);
+}
+
+function draw(){
+background("red")
+fill("yellow")
+textSize(60);
+text("Confugration Of PC",100,50)
+text("1 Motherboard :ROG strix B450-E",100,150)
+text("2    CPU :Ryzen 7 3800XT",100,250)
+text("3    GPU :RTX 2060",100,350)
+text("4    RAM :2 stick of 8 gb ddr 4 3200MHz ",100,450)
+text("5    SSD :500 gb",100,550)
+text("6    HDD :1 TB ",100,650)
+text("7    PSU ",100,750)
+text("8 Coller :Water Coling With 2 Fans",100,850)
+text("9Cabinet : lian li Lancool 215 X  ",100,950)
+}
